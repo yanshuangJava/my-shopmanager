6209ac1dcf07d685cd943c8f18d221ad090a3540
yanshuangjava <564141062@qq.com>
2019-07-28 15:11:38 +0800

    自动生成mapper

---
+++ myshop-databases/src/main/java/com/sitech/myshopdatabases/App.java
@@ -0,0 +1,13 @@
+package com.sitech.myshopdatabases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
