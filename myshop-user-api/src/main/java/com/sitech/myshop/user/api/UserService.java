29a219dadb0f0aebbd0c1f38e604dce6c78b28ad
yanshuangjava <564141062@qq.com>
2019-08-01 23:47:27 +0800

    添回查询所有用户接口

---
+++ myshop-user-api/src/main/java/com/sitech/myshop/user/api/UserService.java
@@ -0,0 +1,11 @@
+package com.sitech.myshop.user.api;
+
+import java.util.List;
+
+import com.sitech.myshopcommondomain.SysUserInfo;
+
+public interface UserService {
+
+	
+	public List<SysUserInfo> selectAll();
+}
